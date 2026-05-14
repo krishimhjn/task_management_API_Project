a92398ffc87fa3c950743cb955da19fb6f5316ee
krishi_mahajan <krishimhjn@gmail.com>
2026-05-14 18:20:49 +0530

    reinitialized alembic

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from src.utils.db import Base,engine
 from src.tasks.router import task_routes
 from src.user.router import user_routes
 
-Base.metadata.create_all(engine)
+# this can work
+# Base.metadata.create_all(engine)
 
 app=FastAPI(title="This is my task management app")
 app.include_router(task_routes)
