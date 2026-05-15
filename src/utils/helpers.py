3e16be6012acdd62905165ce92d7f24910e68510
krishi_mahajan <krishimhjn@gmail.com>
2026-05-15 14:56:05 +0530

    upadted helpper.py

--- src/utils/helpers.py
+++ src/utils/helpers.py
@@ -1,4 +1,5 @@
-from fastapi import Request,HTTPException,Depends,status
+from fastapi import HTTPException, Depends, status
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 import jwt
 from src.utils.settings import settings
 from src.user.models import UserModel
@@ -6,23 +7,23 @@ from sqlalchemy.orm import Session
 from jwt.exceptions import InvalidTokenError
 from src.utils.db import get_db
 
+bearer_scheme = HTTPBearer()
 
-def is_authenticated(request:Request,db:Session=Depends(get_db)):
-        try: 
-            token=request.headers.get("authorization")
-            if not token:
-                raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail="you are unuuthhorized")
+def is_authenticated(
+    credentials: HTTPAuthorizationCredentials = Depends(bearer_scheme),
+    db: Session = Depends(get_db)
+):
+    try:
+        token = credentials.credentials  # extracts token automatically
 
-            token=token.split(" ")[-1]
+        data = jwt.decode(token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
+        user_id = data.get("_id")
 
-            data=jwt.decode(token,settings.SECRET_KEY,settings.ALGORITHM)
-            user_id=data.get("_id")
-            user=db.query(UserModel).filter(UserModel.id==user_id).first()
-            if not user:
-                raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail="you are unuuthhorized")
+        user = db.query(UserModel).filter(UserModel.id == user_id).first()
+        if not user:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Unauthorized")
 
-            return user
-        except InvalidTokenError:
-            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail="you are unuuthhorized")
+        return user
 
-
+    except InvalidTokenError:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Unauthorized")
